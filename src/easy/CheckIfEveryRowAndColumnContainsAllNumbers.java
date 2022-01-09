41f512f0eec05a677825ee72bb2de45cc7aba3a9
Nilutpal Gogoi <nilutpalgogoi@Nilutpals-MacBook-Air.local>
2022-01-09 23:46:28 +0530

    Question2133Update

--- src/easy/CheckIfEveryRowAndColumnContainsAllNumbers.java
+++ src/easy/CheckIfEveryRowAndColumnContainsAllNumbers.java
@@ -5,16 +5,25 @@ import java.util.*;
 public class CheckIfEveryRowAndColumnContainsAllNumbers {
 	public static boolean checkValid(int[][] matrix) {
 		for(int i = 0; i<matrix.length; i++) {
-			int res = check(matrix, i);
-			if(res < matrix.length) return false;
+			int hor = checkHorizontal(matrix, i);
+			int ver = checkVertical(matrix, i);
+			if(hor < matrix.length || ver < matrix.length) return false;
 		}
 		return true;
 	}
 	
-	public static int check(int[][] matrix, int row) {
+	public static int checkHorizontal(int[][] matrix, int row) {
 		HashSet<Integer> set = new HashSet<>();
 		for(int i =0; i<matrix[0].length; i++) {
 			set.add(matrix[row][i]);
+		}
+		return set.size();
+	}
+	
+	public static int checkVertical(int[][] matrix, int col) {
+		HashSet<Integer> set = new HashSet<>();
+		for(int i =0; i<matrix.length; i++) {
+			set.add(matrix[i][col]);
 		}
 		return set.size();
 	}
