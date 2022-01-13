d2c7d6925e88850b711b5d4ae1dc3c0f28418a9b
Nilutpal Gogoi <nilutpalgogoi@Nilutpals-MacBook-Air.local>
2022-01-13 20:11:14 +0530

    BinaryTreeLevelOrderTraversalII

--- src/medium/BinaryTreeLevelOrderTraversalII.java
+++ src/medium/BinaryTreeLevelOrderTraversalII.java
@@ -1,5 +1,47 @@
 package medium;
 
-public class BinaryTreeLevelOrderTraversalII {
+import java.util.*;
 
+public class BinaryTreeLevelOrderTraversalII {
+	public static class TreeNode {
+		int val;
+		TreeNode left;
+		TreeNode right;
+		TreeNode() {}
+		TreeNode(int val) { this.val = val; }
+		TreeNode(int val, TreeNode left, TreeNode right) {
+			this.val = val;
+			this.left = left;
+			this.right = right;
+		}
+	}
+    public List<List<Integer>> levelOrderBottom(TreeNode root) {
+        List<List<Integer>> res = new ArrayList<>();
+        if(root == null){
+            return res;
+        }
+        Queue<TreeNode> qu = new ArrayDeque<>();
+        Stack<List<Integer>> temp = new Stack<>();
+        qu.add(root);
+        while(qu.size() != 0) {
+            int size = qu.size();
+        	List<Integer> list = new ArrayList<>();
+        	while(size > 0) {
+        		TreeNode rem = qu.remove();
+        		list.add(rem.val);
+        		if(rem.left != null)
+        			qu.add(rem.left);
+        		if(rem.right != null)
+        			qu.add(rem.right);
+        		size--;
+        	}
+        	temp.add(list);
+        }
+        
+        while(temp.size() != 0) {
+        	res.add(temp.pop());
+        }
+        
+        return res;
+    }
 }
